97285eda5db4095276677ad70647c6e8dbd4e1e6
quadbeats <jo41010@naver.com>
2023-06-02 20:17:37 +0900

    [Fix} FB 구조 변경

--- src/components/TodoListPage.js
+++ src/components/TodoListPage.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Box,
@@ -17,49 +17,51 @@ import {
   StackDivider,
   HStack,
   IconButton,
+  Checkbox
+  
 } from "@chakra-ui/react";
-import { MdDelete, MdAddCircle } from "react-icons/md";
+import { MdDelete, MdAddCircle,MdCheckBox } from "react-icons/md";
 import { fireStore } from "../firbase";
-import { addDoc,updateDoc,deleteDoc, doc,onSnapshot,collection} from 'firebase/firestore';
+import { addDoc, updateDoc, deleteDoc, doc, query, where, onSnapshot, collection } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 
 export default function TodoListPage() {
-  const [todos, setTodos] = useState([]);
-
+  const [subjects, setSubjects] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [newTodoTitle, setNewTodoTitle] = useState("");
-
+  const [newSubjectTitle, setNewSubjectTitle] = useState("");
   const navigate = useNavigate();
-  //console.log(todos)
+
+  const auth = getAuth();
+  const user = auth.currentUser;
 
   useEffect(() => {
-    // Firestore에서 할 일 목록 가져오기
-    const unsubscribe = onSnapshot(collection(fireStore,'todos'),(snapshot) => {
-      const todosData = snapshot.docs.map((doc) => ({
+    const unsubscribe = onSnapshot(collection(fireStore, "STUDY_SUBJECTS"), (snapshot) => {
+      const subjectsData = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      setTodos(todosData);
+      setSubjects(subjectsData);
     });
 
     return () => {
-      // Cleanup 함수: 구독 취소
       unsubscribe();
     };
   }, []);
 
-  const onClickList = (id, title) => {
-    navigate(`detail/${id}`, { state: { title, previousPage: "TodoListPage" } });
+  const onClickList = (subjectId, subjectTitle) => {
+    navigate(`detail/${subjectId}`, { state: { title: subjectTitle, previousPage: "TodoListPage" } });
   };
 
-  const handleAddTodo = () => {
-    const newTodo = {
-      title: newTodoTitle,
-      subTodos: [],
+  const handleAddSubject = () => {
+    const newSubject = {
+      title: newSubjectTitle,
+      user_id: user.uid,
     };
 
-    addDoc(collection(fireStore,"todos"), (newTodo))
+    addDoc(collection(fireStore, "STUDY_SUBJECTS"), newSubject)
       .then((docRef) => {
-        setNewTodoTitle("");
+        setNewSubjectTitle("");
         setIsModalOpen(false);
       })
       .catch((error) => {
@@ -67,90 +69,10 @@ export default function TodoListPage() {
       });
   };
 
-  const handleDeleteTodo = (id) => {
-    deleteDoc(doc(fireStore,'todos',id))
-      .catch((error) => {
-        console.error("Error deleting document: ", error);
-      });
-  };
-
-
-  const handleAddSubTodo = async (parentId) => {
-    const newSubTodo = {
-      id: Date.now(),
-      title: "",
-    };
-  
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === parentId) {
-        return {
-          ...todo,
-          subTodos: [...todo.subTodos, newSubTodo],
-        };
-      }
-      return todo;
+  const handleDeleteSubject = (subjectId) => {
+    deleteDoc(doc(fireStore, "STUDY_SUBJECTS", subjectId)).catch((error) => {
+      console.error("Error deleting document: ", error);
     });
-  
-    setTodos(updatedTodos);
-    try {
-      await updateDoc(doc(fireStore, 'todos', parentId), {
-        subTodos: updatedTodos.find((todo) => todo.id === parentId).subTodos,
-      });
-    } catch (error) {
-      console.error("Error updating document: ", error);
-    }
-
-
-
-
-
-  };
-  const handleUpdateSubTodo = (parentId, subTodoId, value) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === parentId) {
-        const updatedSubTodos = todo.subTodos.map((subTodo) => {
-          if (subTodo.id === subTodoId) {
-            return {
-              ...subTodo,
-              title: value,
-            };
-          }
-          return subTodo;
-        });
-  
-        return {
-          ...todo,
-          subTodos: updatedSubTodos,
-        };
-      }
-      return todo;
-    });
-  
-    setTodos(updatedTodos);
-  };
-
-  const handleDeleteSubTodo = async (parentId, subTodoId) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === parentId) {
-        const updatedSubTodos = todo.subTodos.filter((subTodo) => subTodo.id !== subTodoId);
-  
-        return {
-          ...todo,
-          subTodos: updatedSubTodos,
-        };
-      }
-      return todo;
-    });
-  
-    setTodos(updatedTodos);
-  
-    try {
-      await updateDoc(doc(fireStore, 'todos', parentId), {
-        subTodos: updatedTodos.find((todo) => todo.id === parentId).subTodos,
-      });
-    } catch (error) {
-      console.error("Error updating document: ", error);
-    }
   };
 
   return (
@@ -162,40 +84,23 @@ export default function TodoListPage() {
           </Text>
           <Box>
             <IconButton mb="1" colorScheme="green" icon={<MdAddCircle />} onClick={() => setIsModalOpen(true)}>
-              Add Todo
+              Add Subject
             </IconButton>
           </Box>
         </HStack>
 
         <List spacing={6}>
-          {todos.map((todo) => (
-            <ListItem key={todo.id} width={64}>
+          {subjects.map((subject) => (
+            <ListItem key={subject.id} width={64}>
               <Box bg={"green.300"} overflow={"hidden"} rounded={"xl"} shadow={"lg"}>
                 <HStack justifyContent={"space-between"} py={"5"} px={"5"}>
-                  <Text as={"b"} onClick={() => onClickList(todo.id, todo.title)}>
-                    {todo.title}
+                  <Text as={"b"} onClick={() => onClickList(subject.id, subject.title)}>
+                    {subject.title}
                   </Text>
-                  <IconButton colorScheme="red" size="lg" ml={2} onClick={() => handleDeleteTodo(todo.id)} icon={<MdDelete />} />
+                  <IconButton colorScheme="red" size="lg" ml={2} onClick={() => handleDeleteSubject(subject.id)} icon={<MdDelete />} />
                 </HStack>
 
-                <VStack spacing={2}>
-                  {todo.subTodos.map((subTodo) => (
-                    <TodoItem
-                      key={subTodo.id}
-                      parentTodoId={todo.id}
-                      subTodo={subTodo}
-                      handleUpdateSubTodo={handleUpdateSubTodo}
-                      handleDeleteSubTodo={handleDeleteSubTodo}
-                    />
-                  ))}
-                  <IconButton
-                    colorScheme="green"
-                    icon={<MdAddCircle />}
-                    onClick={() => handleAddSubTodo(todo.id)}
-                  >
-                    Add Sub Todo
-                  </IconButton>
-                </VStack>
+                <TodoSubList subjectId={subject.id} />
               </Box>
             </ListItem>
           ))}
@@ -205,12 +110,12 @@ export default function TodoListPage() {
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Add Todo</ModalHeader>
+          <ModalHeader>Add Subject</ModalHeader>
           <ModalBody>
-            <Input value={newTodoTitle} onChange={(e) => setNewTodoTitle(e.target.value)} placeholder="Todo title" />
+            <Input value={newSubjectTitle} onChange={(e) => setNewSubjectTitle(e.target.value)} placeholder="Subject title" />
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="green" mr={3} onClick={handleAddTodo}>
+            <Button colorScheme="green" mr={3} onClick={handleAddSubject}>
               Add
             </Button>
             <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
@@ -221,18 +126,92 @@ export default function TodoListPage() {
   );
 }
 
-const TodoItem = ({ parentTodoId, subTodo, handleUpdateSubTodo, handleDeleteSubTodo }) => {
-  const handleUpdate = (e) => {
-    handleUpdateSubTodo(parentTodoId, subTodo.id, e.target.value);
+const TodoSubList = ({ subjectId }) => {
+  const [todos, setTodos] = useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
+  useEffect(() => {
+    const q = query(collection(fireStore, "TODO_LISTS"), where("subjectId", "==", subjectId));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const todosData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTodos(todosData);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [subjectId]);
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      subjectId,
+      title: newTodoTitle,
+      timestamp: Date(),
+      // subTodos: [],
+    };
+
+    addDoc(collection(fireStore, "TODO_LISTS"), newTodo)
+      .then((docRef) => {
+        setNewTodoTitle(""); // 입력 필드 초기화
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
   };
 
+  const handleDeleteTodo = (todoId) => {
+    deleteDoc(doc(fireStore, "TODO_LISTS", todoId)).catch((error) => {
+      console.error("Error deleting document: ", error);
+    });
+  };
+
+  return (
+    <VStack spacing={2}>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleDeleteTodo={handleDeleteTodo}
+        />
+      ))}
+      <HStack>
+        <Input
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+          placeholder="Todo title"
+        />
+        <IconButton colorScheme="green" icon={<MdAddCircle />} onClick={handleAddTodo}>
+          Add Todo
+        </IconButton>
+      </HStack>
+    </VStack>
+  );
+};
+
+const TodoItem = ({ todo, handleDeleteTodo }) => {
   const handleDelete = () => {
-    handleDeleteSubTodo(parentTodoId, subTodo.id);
+    handleDeleteTodo(todo.id);
+  };
+
+  const handleToggleComplete = () => {
+    const updatedTodo = {
+      ...todo,
+      completed: !todo.completed,
+    };
+
+    updateDoc(doc(fireStore, "TODO_LISTS", todo.id), updatedTodo)
+      .catch((error) => {
+        console.error("Error updating document: ", error);
+      });
   };
 
   return (
     <HStack>
-      <Input value={subTodo.title} onChange={handleUpdate} placeholder="Sub Todo title" />
+      <Checkbox size="lg" defaultChecked={todo.completed} onChange={handleToggleComplete} />
+      <Text>{todo.title}</Text>
       <IconButton colorScheme="red" size="lg" ml={2} onClick={handleDelete} icon={<MdDelete />} />
     </HStack>
   );
